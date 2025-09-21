a0efb55e498b5b452222f09226102e900149a4c7
Chu4hel <106600877+Chu4hel@users.noreply.github.com>
2025-09-21 13:01:37 +0300

    feat: Добавить автоматическую генерацию sitemap.xml
    - Установлен и настроен vite-plugin-sitemap.
    - Карта сайта теперь будет автоматически генерироваться при сборке проекта (npm run build).
    - В конфигурацию добавлены основные динамические маршруты приложения.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://www.кумский-сервис.рф',
+      dynamicRoutes: [
+        '/about',
+        '/booking',
+        '/contact',
+        '/services',
+        '/testimonial',
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
