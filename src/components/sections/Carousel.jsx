0c7c21463300fc7451c7d140e74dad7a1efe1f2e
Chu4hel <106600877+Chu4hel@users.noreply.github.com>
2025-10-20 00:26:36 +0300

    feat(carousel): Вынести данные в отдельный файл и адаптировать текст
    Данные для слайдов карусели вынесены из компонента Carousel.jsx в новый файл src/data/carouselData.js.
    - Компонент Carousel.jsx был обновлен для динамического создания слайдов на основе данных из нового файла.
    - Реализовано отображение разного текста для первого слайда на мобильных и десктопных устройствах.

--- src/components/sections/Carousel.jsx
+++ src/components/sections/Carousel.jsx
@@ -10,7 +10,8 @@ import 'swiper/css/effect-fade';
 
 
 // import required modules
-import {Navigation, Pagination, Autoplay, EffectFade} from 'swiper/modules';
+import {Autoplay, EffectFade, Navigation, Pagination} from 'swiper/modules';
+import {carouselData} from '../../data/carouselData.js';
 
 const Carousel = () => {
     return (
@@ -25,79 +26,43 @@ const Carousel = () => {
                 loop={true}
                 effect={'fade'}
             >
-                <SwiperSlide>
-                    <img className="w-100" src="/img/carousel-bg-1.webp" alt="Image"/>
-                    <div className="carousel-caption d-flex align-items-center">
-                        <div className="container">
-                            <div className="row align-items-center justify-content-center justify-content-lg-start">
-                                <div className="col-10 col-lg-7 text-center text-lg-start">
-                                    <h6 className="text-white text-uppercase mb-3 animated slideInDown">// Обслуживание
-                                        автомобилей //</h6>
-                                    <h1 className="display-3 text-white mb-4 pb-3 animated slideInDown">Центр по
-                                        обслуживанию легковых авто</h1>
-                                    <a href="/service" className="btn btn-primary py-3 px-5 animated slideInDown">Узнать
-                                        больше<i className="fa fa-arrow-right ms-3"></i></a>
-                                </div>
-                                <div className="col-lg-5 d-none d-lg-flex animated zoomIn">
-                                    <picture>
-                                        <source srcSet="/img/carousel-1.webp" type="image/webp"/>
-                                        <source srcSet="/img/carousel-1.png" type="image/png"/>
-                                        <img className="img-fluid" src="/img/carousel-1.png" alt=""/>
-                                    </picture>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className="w-100" src="/img/carousel-bg-2.webp" alt="Image"/>
-                    <div className="carousel-caption d-flex align-items-center">
-                        <div className="container">
-                            <div className="row align-items-center justify-content-center justify-content-lg-start">
-                                <div className="col-10 col-lg-7 text-center text-lg-start">
-                                    <h6 className="text-white text-uppercase mb-3 animated slideInDown">// Обслуживание
-                                        автомобилей //</h6>
-                                    <h1 className="display-3 text-white mb-4 pb-3 animated slideInDown">Впервые у нас?
-                                        Диагностика авто в подарок</h1>
-                                    <a href="/service" className="btn btn-primary py-3 px-5 animated slideInDown">Узнать
-                                        больше<i className="fa fa-arrow-right ms-3"></i></a>
-                                </div>
-                                <div className="col-lg-5 d-none d-lg-flex animated zoomIn">
-                                    <picture>
-                                        <source srcSet="/img/carousel-2.webp" type="image/webp"/>
-                                        <source srcSet="/img/carousel-2.png" type="image/png"/>
-                                        <img className="img-fluid" src="/img/carousel-2.png" alt=""/>
-                                    </picture>
+                {carouselData.map((slide) => (
+                    <SwiperSlide key={slide.id}>
+                        <img className="w-100" src={slide.bgImage} alt="Фон карусели"/>
+                        <div className="carousel-caption d-flex align-items-center">
+                            <div className="container">
+                                <div className="row align-items-center justify-content-center justify-content-lg-start">
+                                    <div className="col-10 col-lg-7 text-center text-lg-start">
+                                        <h6 className="text-white text-uppercase mb-3 animated slideInDown">{slide.subtitle}</h6>
+                                        <h1 className="display-3 text-white mb-4 pb-3 animated slideInDown">
+                                            {typeof slide.title === 'object' ? (
+                                                <>
+                                                    <span
+                                                        className="d-none d-lg-block">{slide.title.desktop}</span>
+                                                    <span className="d-lg-none">{slide.title.mobile}</span>
+                                                </>
+                                            ) : (
+                                                slide.title
+                                            )}
+                                        </h1>
+                                        <a href={slide.button.link}
+                                           className="btn btn-primary py-3 px-5 animated slideInDown">
+                                            {slide.button.text}<i className="fa fa-arrow-right ms-3"></i>
+                                        </a>
+                                    </div>
+                                    <div className="col-lg-5 d-none d-lg-flex animated zoomIn">
+                                        <picture>
+                                            <source srcSet={slide.mainImage.webp} type="image/webp"/>
+                                            <source srcSet={slide.mainImage.png} type="image/png"/>
+                                            <img className="img-fluid" src={slide.mainImage.png}
+                                                 alt={slide.mainImage.alt}/>
+                                        </picture>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </SwiperSlide>
-
-                <SwiperSlide>
-                    <img className="w-100" src="/img/carousel-bg-3.webp" alt="Image"/>
-                    <div className="carousel-caption d-flex align-items-center">
-                        <div className="container">
-                            <div className="row align-items-center justify-content-center justify-content-lg-start">
-                                <div className="col-10 col-lg-7 text-center text-lg-start">
-                                    <h6 className="text-white text-uppercase mb-3 animated slideInDown">// Обслуживание
-                                        автомобилей //</h6>
-                                    <h1 className="display-3 text-white mb-4 pb-3 animated slideInDown">Заказываете
-                                        масло у нас? Замена в подарок</h1>
-                                    <a href="/service" className="btn btn-primary py-3 px-5 animated slideInDown">Узнать
-                                        больше<i className="fa fa-arrow-right ms-3"></i></a>
-                                </div>
-                                <div className="col-lg-5 d-none d-lg-flex animated zoomIn">
-                                    <picture>
-                                        <source srcSet="/img/carousel-2.webp" type="image/webp"/>
-                                        <source srcSet="/img/carousel-2.png" type="image/png"/>
-                                        <img className="img-fluid" src="/img/carousel-2.png" alt=""/>
-                                    </picture>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
     );
---
+++ src/data/carouselData.js
@@ -0,0 +1,50 @@
+export const carouselData = [
+    {
+        id: 1,
+        bgImage: '/img/carousel-bg-1.webp',
+        mainImage: {
+            webp: '/img/carousel-1.webp',
+            png: '/img/carousel-1.png',
+            alt: 'Lada Vesta'
+        },
+        subtitle: '// Обслуживание автомобилей //',
+        title: {
+            desktop: 'Квалифицированный центр по обслуживанию легковых авто',
+            mobile: 'Центр по обслуживанию легковых авто'
+        },
+        button: {
+            link: '/service',
+            text: 'Узнать больше'
+        }
+    },
+    {
+        id: 2,
+        bgImage: '/img/carousel-bg-2.webp',
+        mainImage: {
+            webp: '/img/carousel-2.webp',
+            png: '/img/carousel-2.png',
+            alt: 'Проведение диагностики автомобиля'
+        },
+        subtitle: '// Обслуживание автомобилей //',
+        title: 'Впервые у нас? Диагностика авто в подарок',
+        button: {
+            link: '/service',
+            text: 'Узнать больше'
+        }
+    },
+    {
+        id: 3,
+        bgImage: '/img/carousel-bg-3.webp',
+        mainImage: {
+            webp: '/img/carousel-2.webp',
+            png: '/img/carousel-2.png',
+            alt: 'Замена масла в двигателе'
+        },
+        subtitle: '// Обслуживание автомобилей //',
+        title: 'Заказываете масло у нас? Замена в подарок',
+        button: {
+            link: '/service',
+            text: 'Узнать больше'
+        }
+    }
+];
